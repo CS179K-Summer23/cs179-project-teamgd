abc1f401dfa0ca0e371222c8795e2f6b1a5bc9c4
Nathan Tran <Merchant7734@yahoo.com>
2023-08-02 16:40:01 -0700

    Added Encryption + New Login Authentication

---
+++ src/loginEncryption.py
@@ -0,0 +1,78 @@
+#pip install cryptography
+from cryptography.fernet import Fernet
+
+def usernameQuery():
+ print("Input a username > ", end = "")
+ newUsername = input()
+ usernameCheck(newUsername)
+
+
+def usernameCheck(newUsername):
+ with open('login.txt', 'r') as usernameConfirm:
+  lines = usernameConfirm.readlines()
+  for usernameLine in lines[::2]:
+   if usernameLine == newUsername + "\n":
+    print("Username is invalid. Please provide different username.")
+    usernameQuery()
+    return
+  print("Creating new username...")
+  usernameConfirm.close()
+  passwordQuery(newUsername)
+
+
+def passwordQuery(newUsername):
+ print("Input a password > ", end = "")
+ newPassword = input()
+ print("Creating new password...")
+ createLogin(newUsername, newPassword)
+
+
+def createLogin(newUsername, newPassword):
+ appendInput(newUsername)
+ appendInput(newPassword)
+ print("New login successfully created!")
+
+
+def appendInput(userInput):
+ with open('login.txt', 'a') as loginKey:
+  loginKey.write(str(userInput))
+  loginKey.write("\n")
+  loginKey.close()
+
+
+def loginEncrypt():
+ with open('filekey.key', 'rb') as filekey:
+  key = filekey.read()
+ fernet = Fernet(key)
+
+ with open('login.txt', 'rb') as file:
+  original = file.read()
+
+ encrypted = fernet.encrypt(original)
+ with open('login.txt', 'wb') as encryptedFile:
+  encryptedFile.write(encrypted)
+
+ filekey.close()
+ file.close()
+ encryptedFile.close()
+
+
+def loginDecrypt():
+ with open('filekey.key', 'rb') as filekey:
+  key = filekey.read()
+ fernet = Fernet(key)
+
+ with open('login.txt', 'rb') as encryptedFile:
+  encrypted = encryptedFile.read()
+
+ original = fernet.decrypt(encrypted)
+ with open('login.txt', 'wb') as file:
+  file.write(original)
+
+ filekey.close()
+ encryptedFile.close()
+ file.close()
+ 
+loginDecrypt()
+usernameQuery()
+loginEncrypt()
