a2fa78870669ac54a4b80e5199c93a669a0adfa6
Jones171 <jtan097@student.ucr.edu>
2023-08-02 15:52:20 -0700

    Added csv -> json

--- uploadDocument.py
+++ uploadDocument.py
@@ -1,7 +1,35 @@
-def uploadDocument(fname):
-    # DEpending on how the data is stored, read the file name and store it as that 
-    f = open(fname, 'r+')
-    print(f.read())
-    f.close()
+import json
+import csv
+import os
 
-uploadDocument("test.txt")
+def convertToJson(csvPath, jsonPath):
+    data = {}
+    index = 0
+    
+    with open(csvPath) as csvf:
+        csvReader = csv.DictReader(csvf)
+        for rows in csvReader:
+            # key = rows['a'] 
+            # data[key] = rows
+            data[index] = rows
+            index += 1
+ 
+    with open(jsonPath, 'w') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+    
+    print("Success")
+
+# Takes from local computer to the database
+def uploadDocument(srcPath, destPath):
+    # reads file format and if its not json convert it to json
+    split = os.path.splitext(srcPath)
+
+    fileExtension = split[1]
+    print(fileExtension)
+    if fileExtension != '.json' and fileExtension == '.csv':
+        print("Convertind from csv to json")
+        convertToJson(srcPath, destPath)
+    else:
+        print("test")
+
+uploadDocument("test2.csv", "new.json")
