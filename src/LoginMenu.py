6b27be46f7ad0bd5cfeaee51dbf81ef4ed21e35c
Nathan Tran <Merchant7734@yahoo.com>
2023-08-07 14:52:38 -0700

    Added new login menu options

--- src/LoginMenu.py
+++ src/LoginMenu.py
@@ -8,12 +8,10 @@ class Credentials:
 
 
 def printLoginMenu():
-    cred_list = []
+    menu_options = ["Hello, please choose an option", "1. Create New Account", "2. Login", "3. Change Password", "4. Delete User", "5. Exit"]
     while(True):
-        print("Hello, please choose an option")
-        print("1. Create New Account")
-        print("2. Login")
-        print("3. Exit")
+        for option in menu_options:
+            print(option)
         print("Select an option: ", end = "")
         choice = input()
         if choice == "1": #Create New Account
@@ -27,17 +25,21 @@ def printLoginMenu():
             password = input()
             loginDecrypt()
             try:
-             if(login(user, password)):
-              print("Login Successful!")
-              loginEncrypt()
-              printMenu()
-             else:
-              print("Username/Password Invalid!")
-              loginEncrypt()
+                 if(login(user, password)):
+                      print("Login Successful!")
+                      loginEncrypt()
+                      printMenu()
+                 else:
+                      print("Username/Password Invalid!")
+                      loginEncrypt()
             except:
-             print("Unknown error has occurred!")
-             loginEncrypt()
-        elif choice == "3": #Exit
+                 print("Unknown error has occurred!")
+                 loginEncrypt()
+        elif choice == "3": #Change Password
+            print("This feature hasn't been implemented yet...")
+        elif choice == "4": #Delete User
+            print("This feature hasn't been implemented yet...")
+        elif choice == "5": #Exit
             quit()
 
 
