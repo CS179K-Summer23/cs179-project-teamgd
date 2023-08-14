eb91f619c735ea5514a1dda9788050c927299c8c
Benson Wan <tedkim@Bensons-MacBook-Pro-2.local>
2023-08-14 16:20:49 -0700

    update json to csv conversion to work with file name fix

--- src/importjsonDoc.py
+++ src/importjsonDoc.py
@@ -7,6 +7,7 @@ db = Database()
 
 currentpath = os.getcwd()
 parentpath = os.path.dirname(currentpath)
+documentpath = parentpath + "/documents/"
 
 def convertToJson(csvPath, jsonPath):
     data = []
@@ -33,27 +34,18 @@ def convertToJson(csvPath, jsonPath):
     # print("Success")
     
 def convertToCsv(jsonPath):
-    print(jsonPath)
     length = len(jsonPath)
     check = jsonPath[length - 5:]
-    print(check)
         
     if check == ".json":
-        pathElements = jsonPath.split("..") #should be '/'
-        print(pathElements)
+        pathElements = jsonPath.split("/")
         numOfElements = len(pathElements)
-        fileToOpen = ".." + pathElements[numOfElements - 1] #gives ../documents/test.json when it should only be test.json
-        print(fileToOpen)
-        with open(fileToOpen) as json_file:
+        fileToOpen = pathElements[numOfElements - 1]
+        with open(documentpath + fileToOpen) as json_file:
             jsonData = json.load(json_file)
     
-        inputSplit = pathElements[numOfElements - 1].split("/") #should be '.'
-        print(inputSplit) #should give something like ['test', 'json'] after splitting with '.' but because we have to split with '/' it gives ['', 'documents', 'test.json']
-        #next 3 lines are extra steps that will be taken out but are necessary right now until naming issue is fixed
-        tempLength = len(inputSplit)
-        temp = inputSplit[tempLength - 1]
-        temp2 = temp.split(".")
-        fileName = temp2[0] + ".csv" #should be inputSplit[0]
+        inputSplit = fileToOpen.split(".")
+        fileName = inputSplit[0] + ".csv"
         os.chdir("../documents")
         csvFile = open(fileName, "w")
         csvWriter = csv.writer(csvFile)
@@ -68,8 +60,7 @@ def convertToCsv(jsonPath):
             csvWriter.writerow(data.values())
         
         csvFile.close()
-        #destinationPath = parentpath + "/documents/" + fileName
-        db.addFile(fileName) #doesn't save it to correct directory but it saves
+        db.addFile(fileName)
         print("success!")
         os.chdir("../src")
     else:
@@ -96,6 +87,5 @@ def uploadDocument(srcPath, destPath):
         print("Source File extension not JSON.")
 
 
-convertToJson("../documents/test.csv", "../documents/test.csv")
+# convertToJson("../documents/test.csv", "../documents/test.csv")
 # uploadDocument("test2.json", "new2.json")
-
